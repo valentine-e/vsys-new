3586ea6d1ac8eca5e72ff25bd73a492569d08b07
valentine-e <valia101112@gmail.com>
2023-01-02 22:18:41 +0200

    jsх2

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,41 +1,60 @@
-const dropDownMenu = document.querySelectorAll(".navbar__link");
-let currentDropDownOpen = "";
+// const dropDownMenu = document.querySelectorAll(".navbar__link");
+// let currentDropDownOpen = "";
 
-window.addEventListener("click", onWindowClick);
-dropDownMenu.forEach((element) => {
-  element.addEventListener("click", onDropDownClick);
+// window.addEventListener("click", onWindowClick);
+// dropDownMenu.forEach((element) => {
+//   element.addEventListener("click", onDropDownClick);
+// });
+
+// function onDropDownClick(e) {
+//   if (currentDropDownOpen === e.currentTarget) {
+//     closeDropDown();
+//     return;
+//   }
+
+//   if (currentDropDownOpen !== e.currentTarget) {
+//     closeDropDown();
+//   }
+
+//   openDropDown(e);
+// }
+
+// function onWindowClick(e) {
+//   if (
+//     !e.target.classList.contains("navbar-dropdown__link") &&
+//     !e.target.classList.contains("navbar__link")
+//   ) {
+//     closeDropDown();
+//   }
+// }
+
+// function closeDropDown() {
+//   if (currentDropDownOpen) {
+//     currentDropDownOpen.classList.remove("navbar__item--open");
+//     currentDropDownOpen = "";
+//   }
+// }
+
+// function openDropDown(e) {
+//   e.currentTarget.classList.add("navbar__item--open");
+//   currentDropDownOpen = e.currentTarget;
+// }
+
+const dropdownMenu = document.querySelectorAll(".navbar__item");
+
+dropdownMenu.forEach((item) => {
+  item.addEventListener("click", onDropdownClick);
+  item.addEventListener("mouseleave", onMouseLeave);
 });
 
-function onDropDownClick(e) {
-  if (currentDropDownOpen === e.currentTarget) {
-    closeDropDown();
-    return;
-  }
-
-  if (currentDropDownOpen !== e.currentTarget) {
-    closeDropDown();
-  }
-
-  openDropDown(e);
+function onDropdownClick(e) {
+  e.currentTarget
+    .querySelector(".navbar__link")
+    .classList.add("navbar__item--open");
 }
 
-function onWindowClick(e) {
-  if (
-    !e.target.classList.contains("navbar-dropdown__link") &&
-    !e.target.classList.contains("navbar__link")
-  ) {
-    closeDropDown();
-  }
-}
-
-function closeDropDown() {
-  if (currentDropDownOpen) {
-    currentDropDownOpen.classList.remove("navbar__item--open");
-    currentDropDownOpen = "";
-  }
-}
-
-function openDropDown(e) {
-  e.currentTarget.classList.add("navbar__item--open");
-  currentDropDownOpen = e.currentTarget;
+function onMouseLeave(e) {
+  e.currentTarget
+    .querySelector(".navbar__link")
+    .classList.remove("navbar__item--open");
 }
