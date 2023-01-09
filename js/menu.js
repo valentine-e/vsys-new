f4943c829a790275bf70d012a2e023a8ae14179f
valentine-e <valia101112@gmail.com>
2023-01-09 22:58:16 +0200

    22.58

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,8 @@
   const mobileMenuRef = document.querySelector("[data-menu]");
 
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
     document.body.classList.toggle("mobile-open");
     menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
   });
