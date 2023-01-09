d810f97f13cc2870cabb0541bc9b69edd3a595ac
valentine-e <valia101112@gmail.com>
2023-01-09 18:30:14 +0200

    0901

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    document.body.classList.toggle("mobile-open");
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
