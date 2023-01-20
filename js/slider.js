bd0661c634dcfb0d5310d3360868cb82f1583768
valentine-e <valia101112@gmail.com>
2023-01-20 15:58:35 +0200

    slider

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,26 @@
-const sliderContent = document.querySelector(".slider-content");
-const leftBtn = document.querySelector("#left");
-const rightBtn = document.querySelector("#right");
+// const sliderContent = document.querySelector(".slider-content");
+// const leftBtn = document.querySelector("#left");
+// const rightBtn = document.querySelector("#right");
 
-leftBtn.addEventListener("click", moveLeft);
-rightBtn.addEventListener("click", moveRight);
+// leftBtn.addEventListener("click", moveLeft);
+// rightBtn.addEventListener("click", moveRight);
 
-let currentPosition = 0;
-const countSlides = sliderContent.children.length;
+// let currentPosition = 0;
+// const countSlides = sliderContent.children.length;
 
-function moveLeft() {
-  if (currentPosition > -((countSlides - 1) * 100)) {
-    currentPosition -= 100;
-  }
-  sliderContent.style.transform = `translateX(${currentPosition}%)`;
-}
+// function moveLeft() {
+//   if (currentPosition > -((countSlides - 1) * 100)) {
+//     currentPosition -= 100;
+//   }
+//   sliderContent.style.transform = `translateX(${currentPosition}%)`;
+// }
 
-function moveRight() {
-  if (currentPosition < 0) {
-    currentPosition += 100;
-  }
-  sliderContent.style.transform = `translateX(${currentPosition}%)`;
-}
+// function moveRight() {
+//   if (currentPosition < 0) {
+//     currentPosition += 100;
+//   }
+//   sliderContent.style.transform = `translateX(${currentPosition}%)`;
+// }
 
 // setInterval(autoScroll, 3000);
 
@@ -33,3 +33,29 @@ function moveRight() {
 
 //   moveLeft();
 // }
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
